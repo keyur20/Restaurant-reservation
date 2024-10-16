8f9a60c122a0c0b7fc6737ecf22214c8a877b305
keyur20 <keyur.n.patel100@gmail.com>
2024-10-17 02:27:54 +0530

    tokern

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='row py-5 px-5'>
         <article className='col article-footer text-center'>
-          <h5>Urban Masala</h5>
+          <h5>Dragon Masala</h5>
           <p>© 2024 The Company Dragon Masala International Corporation. All rights reserved. The name Urban Masala, logos, images and related marks are registered trademarks of The Company LFF International Corporation for India.</p>
         </article>
     </footer>
